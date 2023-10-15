a6d6ae5fe96b8bafd7b6c4eb854cf555bbb5fc84
himanshu sharma <iamonlyhimanshusharma@gmail.com>
2023-10-15 15:32:05 +0530

    added chemistry cde

--- js/index.js
+++ js/index.js
@@ -6,5 +6,14 @@ document.getElementsByClassName('btn')[0].onclick = ()=>{
     localStorage.clear();
     localStorage.setItem("flask",flask.value)
     localStorage.setItem("lang",lang.value)
+
+    if(flask.value =="a")
+    location.href = './html/index1.html'
+    else if(flask.value =="b")
+    location.href = './html/index2.html'
+    else if(flask.value =="c")
+    location.href = './html/index3.html'
+    else
+    location.href = './html/index4.html'
     
 }
